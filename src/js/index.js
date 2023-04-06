0749639fe35a82f739bc5d72b1f087d4409e0add
Clarissa de Cássia <clari.developer@gmail.com>
2023-04-06 19:45:35 -0300

    add js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
+const formulario = document.getElementById("form");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const telefone = document.getElementById("telefone");
+const mensagem = document.getElementById("mensagem");
 
-let inputs = document.querySelectorAll('input');
 
-
+formulario.addEventListener("submit",function(event) {
+    event.preventDefault();
+    if (nome.value === "") {
+        nome.classList.add('invalido')
+        console.log('batata')
+    }
+    
+})
 
