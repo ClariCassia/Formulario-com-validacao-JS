dd88cab4b560f9a628df63549663c8ef621ba79e
Clarissa de Cássia <clari.developer@gmail.com>
2023-04-08 18:48:57 -0300

    refatoração js

--- src/js/index.js
+++ src/js/index.js
@@ -1,62 +1,29 @@
 const formulario = document.getElementById("form");
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const telefone = document.getElementById("telefone");
-const mensagem = document.getElementById("mensagem");
-const mensagemUsuario = document.getElementById("mensagem");
+const inputs = document.querySelectorAll("input, textarea");
 
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
-  valiarInputs();
+  validaCampoVazio(inputs);
 });
 
-function valiarInputs() {
-  const nomeValue = nome.value;
-  const emailValue = email.value;
-  const telefoneValue = telefone.value;
-  const mensagemUsuarioValue = mensagemUsuario.value;
-
-  if (nomeValue === "") {
-    validaCampoInvalido(nome);
-  } else {
-    validaCampoValido(nome);
-  }
-  if (emailValue === "") {
-    validaCampoInvalido(email);
-  } else {
-    validaCampoValido(email);
-  }
-  if (telefoneValue === "") {
-    validaCampoInvalido(telefone);
-  } else {
-    validaCampoValido(telefone);
-  }
-  if (mensagemUsuarioValue === "") {
-    validaCampoInvalido(mensagemUsuario);
-  } else {
-    validaCampoValido(mensagemUsuario);
-  }
-//   validaFormulario(formulario)
+function validaCampoVazio(inputs) {
+  inputs.forEach((item) => {
+    if (item.value === "") {
+      const controleFormulario = item.parentElement;
+      const mensagemERRO = controleFormulario.querySelector(".msg-obrigatorio");
+      mensagemERRO.classList.add("msg-obrigatorio-visible");
+      item.classList.add("invalido");
+    } else {
+      item.classList.add("valido");
+    }
+  });
 }
 
-function validaCampoInvalido(campo) {
-  const controleFormulario = campo.parentElement;
-  const mensagemERRO = controleFormulario.querySelector(".msg-obrigatorio");
-  mensagemERRO.classList.add("msg-obrigatorio-visible");
-  campo.classList.add("invalido");
+function somenteNumeros(e) {
+  var code = e.which || e.keyCode;
+
+  if (code < 48 || code > 57) {
+    e.preventDefault();
+    return false;
+  }
 }
-
-function validaCampoValido(campo) {
-  campo.classList.add("valido");
-}
-
-// function validaFormulario(formulario) {
-//   const inputsValidos = formulario.querySelectorAll(".valido");
-//   const formularioValido = [...inputsValidos].every((inputsValidos) => {
-//     return (inputsValidos.className = "controle-formulario valido ");
-//   });
-
-//   if (formularioValido) {
-//     alert("Formulario enviado com sucesso");
-//   }
-// }
