ebd5bd2436df08c2295a01f891a8fdd8bbed981c
Clarissa de Cássia <clari.developer@gmail.com>
2023-04-08 11:09:16 -0300

    add js

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,34 @@ const email = document.getElementById("email");
 const telefone = document.getElementById("telefone");
 const mensagem = document.getElementById("mensagem");
 
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  valiarInputs();
+});
 
-formulario.addEventListener("submit",function(event) {
-    event.preventDefault();
-    if (nome.value === "") {
-        nome.classList.add('invalido')
-        console.log('batata')
-    }
-    
-})
+function valiarInputs() {
+  const nomeValue = nome.value;
+  const emailValue = email.value;
+  const telefoneValue = telefone.value;
 
+  if (nomeValue === "") {
+    validaCampoInvalido(nome);
+  } else {
+    validaCampoValido(nome);
+  }
+  if (emailValue === "") {
+    validaCampoInvalido(email);
+  } else {
+    validaCampoValido(email);
+  }
+
+} 
+function validaCampoInvalido(campo) {
+  const msgCampoInvalido = document.querySelector(".msg-obrigatorio");
+  msgCampoInvalido.classList.add("msg-obrigatorio-visible");
+  campo.classList.add("invalido");
+}
+
+function validaCampoValido(campo) {
+  nome.classList.add("valido");
+}
