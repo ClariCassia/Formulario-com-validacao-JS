3fd7ddad88453c727087923b00132a34d0069c7b
Clarissa de Cássia <clari.developer@gmail.com>
2023-06-20 17:19:16 -0300

    refatoração projeto

--- src/js/index.js
+++ src/js/index.js
@@ -3,19 +3,19 @@ const inputs = document.querySelectorAll("input, textarea");
 
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
-  validaCampoVazio(inputs);
+  validaCamposVazios(inputs);
 });
 
-function validaCampoVazio(inputs) {
+function validaCamposVazios(inputs) {
   inputs.forEach((input) => {
     const campoFormulario = input.parentElement;
-    const mensagemERRO = campoFormulario.querySelector(".msg-campos-obrigatorio");
+    const mensagemErro = campoFormulario.querySelector(".msg-campos-obrigatorio");
     if (input.value === "") {
-      mensagemERRO.classList.add("msg-obrigatorio-visible");
+      mensagemErro.classList.add("msg-obrigatorio-visible");
       input.classList.add("invalido");
       input.classList.remove("valido");
     } else {
-      mensagemERRO.classList.remove("msg-obrigatorio-visible");
+      mensagemErro.classList.remove("msg-obrigatorio-visible");
       input.classList.add("valido");
       input.classList.remove("invalido");
     }
@@ -23,8 +23,7 @@ function validaCampoVazio(inputs) {
 }
 
 function somenteNumeros(e) {
-  var code = e.which || e.keyCode;
-
+  const code = e.which || e.keyCode;
   if (code < 48 || code > 57) {
     e.preventDefault();
     return false;
