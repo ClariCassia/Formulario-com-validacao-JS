ff2e062b99525e6cbb5ef1abd87b55d218b8fa14
Clarissa de Cássia <clari.developer@gmail.com>
2023-07-03 20:14:05 -0300

    refatoração do js

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,22 @@
 const formulario = document.getElementById("form");
 const inputs = document.querySelectorAll("input, textarea");
+const mensagensErro = document.querySelectorAll(".msg-campos-obrigatorio");
 
-formulario.addEventListener("submit", function (event) {
-  event.preventDefault();
-  validaCamposVazios(inputs);
-});
 
-function validaCamposVazios(inputs) {
-  inputs.forEach((input) => {
-    const campoFormulario = input.parentElement;
-    const mensagemErro = campoFormulario.querySelector(".msg-campos-obrigatorio");
-    if (input.value === "") {
-      mensagemErro.classList.add("msg-obrigatorio-visible");
-      input.classList.add("invalido");
-      input.classList.remove("valido");
-    } else {
-      mensagemErro.classList.remove("msg-obrigatorio-visible");
-      input.classList.add("valido");
-      input.classList.remove("invalido");
-    }
-  });
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validaCampos()
+})
+
+const validaCampos = () => {
+  inputs.forEach((input, index) => {
+    input.value === "" ?
+      (input.classList.add("invalido"), mensagensErro[index].classList.add("msg-obrigatorio-visible")) :
+      (input.classList.remove("invalido"), input.classList.add("valido"), mensagensErro[index].classList.remove("msg-obrigatorio-visible"));
+  })
 }
 
-function somenteNumeros(e) {
-  const code = e.which || e.keyCode;
-  if (code < 48 || code > 57) {
-    e.preventDefault();
-    return false;
-  }
+function validarTelefone(input) {
+  input.value = input.value.replace(/[^0-9]/g, ''); 
+  input.value = input.value.replace(/,/g, ''); 
 }
